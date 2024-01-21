ac3a11055b5889616b6da8a39451e32198c9d13b
SShehan <sachinshehan20@gmail.com>
2024-01-21 14:35:45 +0530

    top bar and side bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import TopBar from './components/TopBar';
+import SideBar from './components/SideBar';
 import Dashboard from './pages/Dashboard';
 
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-
-
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (    
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+        <SideBar isSidebar={isSidebar} />
+          <main className='content'>
+          <TopBar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
